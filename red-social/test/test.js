ca8e55dcec2060c101ba968e56770d988f61f722
Kelvin Jose Peralta Almonte <kjpa1315@gmail.com>
2022-12-21 22:57:58 -0500

    modificando la seguridad de database node_env

--- red-social/test/test.js
+++ red-social/test/test.js
@@ -1,6 +1,23 @@
 const { assert } = require('chai')
-const { it, describe} = require('mocha')
+const { it, describe } = require('mocha')
 
-describe('estos son los tests de suma', () => {
-    it("Dberia retornarse al sumar 5 y 8")
+const sum = (a, b) => {
+    return a + b
+}
+
+sum(5,8) //? -> 13
+sum(50, 9) //? -> 59
+sum(16, 24) //? -> 40
+
+describe('Estos son los tests de suma', () => {
+    it('Deberia retornar 13 al sumar 5 y 8', (done) => {
+        const myFunc = sum(5, 8)
+        assert.equal(myFunc, 13)
+        done()
+    })
+    it('Deberia retornar 59 al sumar 50 y 9', (done) => {
+        const myFunc = sum(50, 9)
+        assert.equal(myFunc, 59)
+        done()
+    })
 })
